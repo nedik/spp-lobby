9079699e6d75dda8d10d7c9845974e18ccaee88d
nedik <kknedi@gmail.com>
2024-08-05 19:19:08 +0200

    Add environment variable reader

---
+++ initializers/loadEnv.go
@@ -0,0 +1,27 @@
+package initializers
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ServerPort      string `mapstructure:"PORT"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
+
+
--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"log"
+
+	"github.com/nedik/spp-lobby/initializers"
 	"github.com/nedik/spp-lobby/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+    config, err := initializers.LoadConfig(".")
+    if err != nil {
+        log.Fatal("Could not load environment variables", err)
+    }
+
 	router := gin.Default()
 
 	routes.InitServerRoutes(router)
     routes.InitFaviconRoutes(router)
 
-	router.Run()
+    log.Fatal(router.Run(":" + config.ServerPort))
 }
